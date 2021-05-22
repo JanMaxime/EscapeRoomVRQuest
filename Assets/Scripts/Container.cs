2c968c8a63f5c8d70991df7ef0af0c83ba7148a9
Jan Maxime <maxime.jan@epfl.ch>
2021-05-22 17:31:53 +0200

    Enhance fill container

--- Assets/Scripts/Container.cs
+++ Assets/Scripts/Container.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Container : MonoBehaviour
 {
     private int currentWeights = 0;
     private int totalWeights = 5;
 
+    public Text text;
+
+    public Transform weights_initial_parent;
+
+    void Start(){
+        updateText();
+    }
+
+    void updateText(){
+        text.text = currentWeights + "/" + totalWeights + " ton";
+    }
+
     void OnTriggerEnter(Collider other){
         if (other.tag == "weight"){
+            other.transform.parent = this.transform;
             currentWeights += 1;
+            updateText();
         }
     }
 
     void OnTriggerExit(Collider other){
         if (other.tag == "weight"){
+            other.transform.parent = weights_initial_parent;
             currentWeights -= 1;
+            updateText();
         }
     }
     public bool isFull(){
